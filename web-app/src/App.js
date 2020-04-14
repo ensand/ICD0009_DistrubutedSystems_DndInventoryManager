38af7b241c1d7411e7dbd1de85bb7a9ff31856ea
Enola Sander <enola1998@gmail.com>
2020-04-14 17:55:43 +0300

    Updated frontend

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,10 +1,56 @@
 import React from 'react';
 import './App.css';
 
+import Main from './Views/Main/Main.jsx';
+
 function App() {
     return (
-        <div>
-            Hello world!
+        <div className="main">
+            <header>
+                <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+                    <div class="container">
+                        <a class="navbar-brand" asp-area="" asp-controller="Home" asp-action="Index">WebApp</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
+                            <partial name="_LoginPartial" />
+                            <ul class="navbar-nav flex-grow-1">
+                                <li class="nav-item">
+                                    <a class="nav-link text-dark">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-dark">Characters</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-dark">Armor</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-dark">Weapons</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-dark">Magical items</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-dark">Other equipment</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <div class="container">
+                <main role="main" class="pb-3">
+                    <Main />
+                </main>
+            </div>
+
+            <footer class="border-top footer text-muted">
+                <div class="container">
+                    &copy; 2020 - WebApp - <a asp-area="" asp-controller="Home" asp-action="Privacy">Privacy</a>
+                </div>
+            </footer>
         </div>
     );
 }
---
+++ web-app/src/Views/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main(props) {
+    return (
+        <div className="text-center">
+            <h1 className="display-4">Welcome</h1>
+            <p>Learn about <a href="https://docs.microsoft.com/aspnet/core">building Web apps with ASP.NET Core</a>.</p>
+            <p>Learn about <a href="https://reactjs.org/docs/create-a-new-react-app.html">creating JavaScript apps with React</a>.</p>
+        </div>
+    );
+}
+
+export default Main;

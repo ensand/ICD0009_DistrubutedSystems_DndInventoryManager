f359457d5fe0a7d9ee804a5d5a4addcb66c3384e
Enola Sander <enola1998@gmail.com>
2020-05-18 00:00:23 +0300

    Added character overview

--- web-app/src/Views/DndCharacters/View.jsx
+++ web-app/src/Views/DndCharacters/View.jsx
@@ -5,7 +5,7 @@ import {useStoreState} from 'easy-peasy';
 import {Grid, Paper, Typography} from '@material-ui/core';
 
 function View() {
-
+    const userIsLoggedIn = useStoreState(state => state.appState.userLoggedIn);
     const token = useStoreState(state => state.appState.token);
 
     const [items, setItems] = React.useState([]);
@@ -36,7 +36,7 @@ function View() {
 
     React.useEffect(() => {
         fetchItems();
-    }, []);
+    }, [userIsLoggedIn]);
 
     return (
         <div>
@@ -45,52 +45,24 @@ function View() {
             <Grid container>
                 {items.map((item) => {
                     return <Grid item key={item.id} style={{minHeight: "10vh", minWidth: "15vw", width: "fit-content", height: "fit-content"}}>
-                        <Paper style={{backgroundColor: "lightGray", padding: "1rem"}}>
+                        <Paper style={{backgroundColor: "#ececec", padding: "1rem"}}>
                             <Typography variant="h5">{item.name}</Typography>
-                            {item.comment && <Typography variant="subtitle2">{item.comment}</Typography>}
-                            <Typography variant="body2">key</Typography>
-                            <Typography variant="body1">value</Typography>
+                            {item.comment && <Typography variant="subtitle2"><i>{item.comment}</i></Typography>}
+                            <hr/>
+                            <div style={{display: "grid", gridTemplateColumns: "1fr 1fr", gridTemplateRows: "1fr 1fr 1fr", gridColumnGap: "1rem", justifyItems: "start"}}>
+                                <Typography variant="body2">Weapons</Typography>
+                                <Typography variant="body1" style={{justifySelf: "center"}}>{item.weaponCount}</Typography>
+                                <Typography variant="body2">Armor</Typography>
+                                <Typography variant="body1" style={{justifySelf: "center"}}>{item.armorCount}</Typography>
+                                <Typography variant="body2">Other equipment</Typography>
+                                <Typography variant="body1" style={{justifySelf: "center"}}>{item.otherEquipmentCount + item.magicalItemCount}</Typography>
+                                <Typography variant="body2">Treasure in GP</Typography>
+                                <Typography variant="body1" style={{justifySelf: "center"}}>{item.treasureInGp}</Typography>
+                            </div>
                         </Paper>
                     </Grid>
                 })}
             </Grid>
-            {/* <table className="table">
-                <thead>
-                    <tr>
-                        <th>
-                            Character
-                        </th>
-                        <th>
-                            Total treasure in GP
-                        </th>
-                        <th>
-                            Comment
-                        </th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {items.map((item) => {
-                        return (
-                            <tr key={item.id}>
-                                <td>
-                                    {item.name}
-                                </td>
-                                <td>
-                                    {item.totalTreasureInGp}
-                                </td>
-                                <td>
-                                    {item.comment ? item.comment : "-"}
-                                </td>
-                                <td>
-                                    <button>Edit</button> |
-                                    <button onClick={() => deleteItem(item.id)}>Delete</button>
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table> */}
         </div>
     );
 }
